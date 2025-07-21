55530c2f71a87644c8b1e36c8e92e7a469b754e6
Guda Manjunadha <gudamanjunadhareddy@gmail.com>
2025-07-21 12:04:17 +0530

    sign up completed

--- src/components/Connections.jsx
+++ src/components/Connections.jsx
@@ -23,15 +23,15 @@ const Connections = () => {
   }, []);
 
   if (!connections) return;
-  if (connections.length === 0) return <div>No connections found</div>;
+  if (connections.length === 0) return <h1 className="flex justify-center my-10 text-2xl">No connections found</h1>;
   return (
     <div className="text-center my-10">
       <h2 className="text-bold text-3xl ">Connections</h2>
-      {connections.map((connection) => {
+      {connections.map((connection,index) => {
         const { _id,firstName, lastName, photoUrl, age, gender, about } =
           connection;
         return (
-            <div className="flex m-4 p-4 rounded-lg bg-base-300 w-1/2 mx-auto border-2" key={_id}>
+            <div className="flex m-4 p-4 rounded-lg bg-base-300 w-1/2 mx-auto border-2" key={_id || index}>
                 <div>
                     <img src={photoUrl} alt="photo" className="w-20 h-20 rounded-full object-cover"/>
                 </div>
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -7,24 +7,48 @@ import { BASE_URL } from "../utils/constants";
 const Login = () => {
   const [emailID, setEmailId] = useState("");
   const [password, setPassword] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [isLoginFrom, setIsLoginFrom] = useState(true);
   const [error, setError] = useState("");
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
   const handleLogin = async () => {
     try {
-        const res = await axios.post(
-          BASE_URL + "/login",
-          {
-            emailID,
-            password,
-          },
-          {
-            withCredentials: true,
-          }
-        );
+      const res = await axios.post(
+        BASE_URL + "/login",
+        {
+          emailID,
+          password,
+        },
+        {
+          withCredentials: true,
+        }
+      );
       dispatch(addUser(res.data));
       return navigate("/");
+    } catch (err) {
+      setError(err?.response?.data || "Something went wrong!");
+    }
+  };
+
+  const handleSignup = async () => {
+    try {
+      const res = await axios.post(
+        BASE_URL + "/signup",
+        {
+          firstName,
+          lastName,
+          emailID,
+          password,
+        },
+        {
+          withCredentials: true,
+        }
+      );
+      dispatch(addUser(res?.data?.data));
+      return navigate("/profile");
     } catch (err) {
       setError(err?.response?.data || "Something went wrong!");
     }
@@ -34,7 +58,37 @@ const Login = () => {
     <div className="flex justify-center my-10">
       <div className="card card-border bg-base-300 w-96 shadow-xl">
         <div className="card-body">
-          <h2 className="card-title justify-center">Login</h2>
+          <h2 className="card-title justify-center">
+            {isLoginFrom ? "Login" : "Sign up"}
+          </h2>
+          {!isLoginFrom && (
+            <>
+              <div>
+                <fieldset className="fieldset">
+                  <legend className="fieldset-legend">FirstName</legend>
+                  <input
+                    type="text"
+                    value={firstName}
+                    className="input"
+                    placeholder="First Name"
+                    onChange={(e) => setFirstName(e.target.value)}
+                  />
+                </fieldset>
+              </div>
+              <div>
+                <fieldset className="fieldset">
+                  <legend className="fieldset-legend">LastName</legend>
+                  <input
+                    type="text"
+                    value={lastName}
+                    className="input"
+                    placeholder="Last Name"
+                    onChange={(e) => setLastName(e.target.value)}
+                  />
+                </fieldset>
+              </div>
+            </>
+          )}
           <div>
             <fieldset className="fieldset">
               <legend className="fieldset-legend">Email ID</legend>
@@ -61,10 +115,18 @@ const Login = () => {
           </div>
           <div className="text-red-600">{error}</div>
           <div className="card-actions justify-center">
-            <button className="btn btn-primary" onClick={handleLogin}>
-              Login
+            <button className="btn btn-primary" onClick={isLoginFrom ? handleLogin : handleSignup}>
+              {isLoginFrom ? "Login" : "Sign Up"}
             </button>
           </div>
+          <p
+            className="m-auto cursor-pointer py-2"
+            onClick={() => setIsLoginFrom((value) => !value)}
+          >
+            {isLoginFrom
+              ? "New User? Signup Here"
+              : "Existing User? Login Here"}
+          </p>
         </div>
       </div>
     </div>
